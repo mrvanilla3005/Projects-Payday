00a0ad11be84dac78a5184a5d63fb23e4e193bd1
Dominik <szb9v285wv@privaterelay.appleid.com>
2026-06-08 17:20:35 +0200

    add supabase integration

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,14 @@ import { seedKupony } from './data/seedProdej.js'
 
 patchKupony(seedKupony)
 
-insertSeedData().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-  )
-})
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
+
+insertSeedData()
